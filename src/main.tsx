596989a52d388b1f5042ca0861881b98bd013769
diego <diegolarrieta@gmail.com>
2024-09-30 15:42:24 -0600

    Estructura

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from '../src/components/ejercicios.tsx'
+import Header from './components/header.tsx'
+import Sidebar from './components/siedebar.tsx'
+
 import '../src/components/ejercicios.css'
-
 import './index.css'
-
+import './components/header.css'
+import './components/sidebar.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
