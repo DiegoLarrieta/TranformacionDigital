f94c4483b90319006caf0032ad9887cced1d8827
diego <diegolarrieta@gmail.com>
2024-11-25 14:11:26 -0600

    RedirectsV2

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects', // Archivo fuente que queremos copiar
+          dest: '' // Directorio de destino (la raíz del directorio `dist`)
+        }
+      ]
+    })
+  ],
+});
