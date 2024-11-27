2e70471cbb12b735038eba0f8a7a5ff966d1ebf3
diego <diegolarrieta@gmail.com>
2024-11-27 11:51:45 -0600

    AdaptacionServidorTecV1

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
+  base: '/CCA_cursos/ad2024/', // Establece el subdirectorio base desde el cual se sirve tu proyecto
   plugins: [
     react(),
     viteStaticCopy({
